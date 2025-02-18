9aa14e757ea3e3745feecd206251fff6545a4e4d
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-18 15:16:18 -0300

    add ChatService

---
+++ src/main/java/com/xfef0/springAI/ChatService.java
@@ -0,0 +1,37 @@
+package com.xfef0.springAI;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.mistralai.MistralAiChatOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final ChatModel chatModel;
+
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+
+    public String getResponseWithOptions(String prompt) {
+        ChatResponse response = chatModel.call(
+                new Prompt(
+                        prompt,
+                        MistralAiChatOptions.builder()
+                                .model("mistral-large-latest")
+                                .temperature(0.7)
+                                .maxTokens(500)
+                                .build()
+                )
+        );
+
+        return response.getResult().getOutput().getText();
+    }
+}
