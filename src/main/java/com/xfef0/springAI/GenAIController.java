22617ed5100341f91103583a1d36ef53df5cff1a
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-02-18 15:38:01 -0300

    add generate-image endpoint to controller

--- src/main/java/com/xfef0/springAI/GenAIController.java
+++ src/main/java/com/xfef0/springAI/GenAIController.java
@@ -1,18 +1,24 @@
 package com.xfef0.springAI;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("${api.prefix}")
 public class GenAIController {
 
     private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAIController(ChatService chatService) {
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("/ask-ai")
@@ -23,5 +29,12 @@ public class GenAIController {
     @GetMapping("/ask-ai-options")
     public String getResponseWithOptions(@RequestParam String prompt) {
         return chatService.getResponseWithOptions(prompt);
+    }
+
+    @GetMapping("/generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 }
